3dbc73a0e762ca24eefe501928ce577dd181d3b4
Keshav Sharma <keshavsharma1666@gmail.com>
2026-08-19 15:09:19 +0530

    Personalize hero for Keshav Sharma

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,32 +3,31 @@ import "./styles/Landing.css";
 
 const Landing = ({ children }: PropsWithChildren) => {
   return (
-    <>
-      <div className="landing-section" id="landingDiv">
-        <div className="landing-container">
-          <div className="landing-intro">
-            <h2>Hello! I'm</h2>
-            <h1>
-              MONCY
-              <br />
-              <span>YOHANNAN</span>
-            </h1>
-          </div>
-          <div className="landing-info">
-            <h3>A Creative</h3>
-            <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Designer</div>
-              <div className="landing-h2-2">Developer</div>
-            </h2>
-            <h2>
-              <div className="landing-h2-info">Developer</div>
-              <div className="landing-h2-info-1">Designer</div>
-            </h2>
-          </div>
+    <div className="landing-section" id="landingDiv">
+      <div className="landing-container">
+        <div className="landing-intro">
+          <h2>Hello! I'm</h2>
+          <h1>
+            KESHAV
+            <br />
+            <span>SHARMA</span>
+          </h1>
         </div>
-        {children}
+
+        <div className="landing-info">
+          <h3>A Data</h3>
+          <h2 className="landing-info-h2">
+            <div className="landing-h2-1">Analyst</div>
+            <div className="landing-h2-2">BI Professional</div>
+          </h2>
+          <h2>
+            <div className="landing-h2-info">Business Intelligence</div>
+            <div className="landing-h2-info-1">Data Analyst</div>
+          </h2>
+        </div>
       </div>
-    </>
+      {children}
+    </div>
   );
 };
 
