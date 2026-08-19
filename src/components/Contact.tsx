93eec60b0236d472584d2b4e87e7aa5ee359a01e
Keshav Sharma <keshavsharma1666@gmail.com>
2026-08-19 15:09:56 +0530

    Personalize contact for Keshav

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,58 +10,28 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Email</h4>
             <p>
-              <a href="mailto:example@mail.com" data-cursor="disable">
-                example@mail.com
+              <a href="mailto:keshavsharma1666@gmail.com" data-cursor="disable">
+                keshavsharma1666@gmail.com
               </a>
             </p>
-            <h4>Phone</h4>
-            <p>
-              <a href="tel:+9199999999" data-cursor="disable">
-                +91 99999 99999
-              </a>
-            </p>
+            <h4>Location</h4>
+            <p>India</p>
           </div>
           <div className="contact-box">
             <h4>Social</h4>
-            <a
-              href="https://github.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Github <MdArrowOutward />
+            <a href="https://github.com/Keshavv15" target="_blank" rel="noreferrer" data-cursor="disable" className="contact-social">
+              GitHub <MdArrowOutward />
             </a>
-            <a
-              href="https://www.linkedin.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Linkedin <MdArrowOutward />
-            </a>
-            <a
-              href="https://x.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Twitter <MdArrowOutward />
-            </a>
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Instagram <MdArrowOutward />
+            <a href="https://www.linkedin.com/in/keshav-sharma15" target="_blank" rel="noreferrer" data-cursor="disable" className="contact-social">
+              LinkedIn <MdArrowOutward />
             </a>
           </div>
           <div className="contact-box">
             <h2>
-              Designed and Developed <br /> by <span>Moncy Yohannan</span>
+              Built around <br /> data, <span>insights & impact.</span>
             </h2>
             <h5>
-              <MdCopyright /> 2024
+              <MdCopyright /> 2026 Keshav Sharma
             </h5>
           </div>
         </div>
